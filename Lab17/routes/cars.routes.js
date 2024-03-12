66df2d458d26abe0b74eb17a2f92e53256e516f2
NicoH00d <a01705857@tec.mx>
2024-03-12 12:28:40 -0600

    actualizar lab 17

--- Lab17/routes/cars.routes.js
+++ Lab17/routes/cars.routes.js
@@ -6,7 +6,8 @@ const cochesController = require('../controllers/coches.controller');
 
 router.get('/creacoche', cochesController.get_coches);
 router.post('/creacoche', cochesController.post_coches);
-//router.get('/:coches_id', cochesController.get_miscoches);
 router.get('/coches', cochesController.get_miscoches);
+
+router.get('/coches:coches_id', cochesController.get_miscoches);
 
 module.exports = router;
--- Lab17/sql/coches.sql
+++ Lab17/sql/coches.sql
@@ -1,48 +1,58 @@
--- phpMyAdmin SQL Dump
--- version 6.0.0-dev
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Servidor: 192.168.30.23
--- Tiempo de generación: 11-03-2024 a las 04:06:00
--- Versión del servidor: 8.0.18
--- Versión de PHP: 8.2.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: coches
+-- ------------------------------------------------------
+-- Server version	8.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `coches`
+-- Table structure for table `coches`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `coches`
---
-
+DROP TABLE IF EXISTS `coches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `coches` (
   `Marca` varchar(20) NOT NULL,
   `Modelo` varchar(20) NOT NULL,
   `Motor` varchar(20) NOT NULL,
-  `Imagen` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Imagen` varchar(255) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `coches`
+-- Dumping data for table `coches`
 --
 
-INSERT INTO `coches` (`Marca`, `Modelo`, `Motor`, `Imagen`) VALUES
-('Ferrari', 'F40', 'V10', 'https://hips.hearstapps.com/hmg-prod/images/1991-ferrari-f40-103-1593201414.jpg'),
-('Ferrari', 'F40', 'V10', 'https://hips.hearstapps.com/hmg-prod/images/1991-ferrari-f40-103-1593201414.jpg');
-COMMIT;
+LOCK TABLES `coches` WRITE;
+/*!40000 ALTER TABLE `coches` DISABLE KEYS */;
+INSERT INTO `coches` VALUES ('Ferrari','F40','V10','https://i.pinimg.com/originals/38/a0/b4/38a0b462ad646c7b1426b168101a5ca6.jpg',13),('Lamborghini','Aventador SVJ','V12','https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2018/10/volante-lamborghini-aventador-svj_5.jpg',15),('Porsche','911 GT3 RS','V16','https://wallpapersmug.com/download/1280x960/d85f54/porsche-911-gt3-rs-2019-cgi.jpg',16),('Ford','Mustang shelby 1967','V8','https://imagescdn.dealercarsearch.com/Media/14801/13834963/637154738629164880.jpg',17),('Ford','Ford Shelby Cobra','V8','https://img.favcars.com/shelby/cobra/shelby_cobra_1962_pictures_1.jpg',18),('Mercedes','BRABUS 900','x','https://www.brabus.com/_Resources/Persistent/3/6/f/5/36f5621b55b44ff3648314d6aeab49cd18b0d6ee/20210520_Brabus_Cannonball_event_3483-1170x780.jpg',19),('Nissan','GTR R34','I6 Biturbo','https://www.desktopbackground.org/download/1280x960/2010/11/02/105009_download-wallpapers-nissan-skyline-gtr-gt-r-r34-v-spec-2-nur_1980x1263_h.jpg',22);
+/*!40000 ALTER TABLE `coches` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping routines for database 'coches'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-12 12:12:48
